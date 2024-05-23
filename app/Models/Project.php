af5c4e96975c87603cfed941b52074a51b5693a8
StefanRaluca <raluca.bubulina@yahoo.com>
2024-05-23 14:52:20 +0200

    add foreign key for type id

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,11 +2,19 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Type;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'slug', 'image_cover', 'image_cover', 'description', 'start_date', 'preview_url', 'url_code', 'team_members'];
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
