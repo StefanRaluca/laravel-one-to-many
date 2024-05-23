e05ae7e757809b0654f9799c6fefb045f22e7904
StefanRaluca <raluca.bubulina@yahoo.com>
2024-05-23 13:26:52 +0200

    add model Type with fillable and function hasmany

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Type extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'slug', 'description', 'active'];
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
+
 }
